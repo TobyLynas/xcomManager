86271d0e5b7818665eeceb946c28f7ad9e920365
Toby Lynas <tobylynas@Tobys-Air.lan>
2021-03-18 14:25:17 +0000

    initial commit

---
+++ client/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          Hello world
+        </div>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
