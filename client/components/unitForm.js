14b4ebf7cf2b3713769ed322ccc7a84dff33c52c
Toby Lynas <tobylynas@tobys-air.lan>
2021-03-29 17:45:45 +0100

    get POST working

--- client/components/unitForm.js
+++ client/components/unitForm.js
@@ -1,34 +1,57 @@
 import styles from './unitForm.module.css'
 
-const UnitForm = () => {    
+const UnitForm = () => {
+        const postUnit = async event => {
+            event.preventDefault()
+            const res = await fetch('http://localhost:9000/users',
+            {
+              body: JSON.stringify({
+                name: event.target.soldierName.value,
+                health: event.target.health.value,
+                aim: event.target.aim.value,
+                mobility: event.target.mobility.value,
+                will: event.target.will.value,
+                armour: event.target.armour.value,
+                dodge: event.target.dodge.value,
+                hack: event.target.hack.value,
+                className: event.target.class.value
+              }),
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              method: 'POST'
+            }
+            )
+            location.reload()
+        }
     return (
-            <form className={styles.formMain} onSubmit={console.log('Sumbitted')}>
+            <form /*action={sendData()}*/ onSubmit={postUnit} method="POST" className={styles.formMain}>
                 <label htmlFor="unit">Name:
-                    <input className={styles.nameInput} id="soldierName" type="text" required />
+                    <input className={styles.nameInput} id="soldierName" type="text" required/>
                 </label>
                 <label>Health
-                    <input className={styles.input} type="number" id="health"/>
+                    <input className={styles.input} type="number" id="health" required/>
                 </label>
                 <label>Mobility:
-                    <input className={styles.input} type="number" id="mobility"/>
+                    <input className={styles.input} type="number" id="mobility" required/>
                 </label>
                 <label>Aim:
-                    <input className={styles.input} type="number" id="aim"/>
+                    <input className={styles.input} type="number" id="aim" required/>
                 </label>
                 <label>Will:
-                    <input className={styles.input} type="number" id="will"/>
+                    <input className={styles.input} type="number" id="will" required/>
                 </label>
                 <label>Armour:
-                    <input className={styles.input} type="number" id="armour"/>
+                    <input className={styles.input} type="number" id="armour" required/>
                 </label>
                 <label>Dodge:
-                    <input className={styles.input} type="number" id="dodge"/>
+                    <input className={styles.input} type="number" id="dodge" required/>
                 </label>
                 <label>Hack:
-                    <input className={styles.input} type="number" id="hack"/>
+                    <input className={styles.input} type="number" id="hack" required/>
                 </label>
                 <label> ClassInput:
-                <select className={styles.classInput} id="class">
+                <select className={styles.classInput} id="class" required >
                     <option>Assualt</option>
                     <option>Grenadier</option>
                     <option>Gunner</option>
@@ -40,7 +63,7 @@ const UnitForm = () => {
                     <option>Psi Operative</option>
                 </select>
                 </label>
-                <button className={styles.subButton} type="submit">Submit</button>
+                <button className={styles.subButton} type="submit">Submit!</button>
             </form>
     )}
 
